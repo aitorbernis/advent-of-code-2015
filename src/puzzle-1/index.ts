3a3fb82e5558d4541abc507d40eb9657491ef494
Aitor Bernis <76017299+aitorbernis@users.noreply.github.com>
2022-05-18 20:06:37 +0200

    Day 1

--- src/puzzle-1/index.ts
+++ src/puzzle-1/index.ts
@@ -4,4 +4,16 @@ const args = process.argv.slice(2);
 const isTest = (args || ["0"])[0] === "test" ? true : false;
 const fileName = isTest ? "input-test.txt" : "input.txt";
 
-const inputValues: string[] = fs.readFileSync(path.resolve(__dirname, fileName)).toString().split("\n");
+const inputValues: string = fs.readFileSync(path.resolve(__dirname, fileName)).toString()
+let counter = 0
+const openingBracket = '('
+const closingBracket = '('
+for (let i = 0; i < inputValues.length; i++) {
+  if (inputValues[i] === openingBracket) {
+    counter++
+  } else {
+    counter--
+  }
+}
+
+console.log(counter)
--- src/puzzle-2/index.ts
+++ src/puzzle-2/index.ts
@@ -1,7 +1,21 @@
-import fs = require("fs");
-import path = require("path");
-const args = process.argv.slice(2);
-const isTest = (args || ["0"])[0] === "test" ? true : false;
-const fileName = isTest ? "input-test.txt" : "input.txt";
+import fs = require("fs")
+import path = require("path")
+const args = process.argv.slice(2)
+const isTest = (args || ["0"])[0] === "test" ? true : false
+const fileName = isTest ? "input-test.txt" : "input.txt"
 
-const inputValues: string[] = fs.readFileSync(path.resolve(__dirname, fileName)).toString().split("\n");
+const inputValues: string = fs.readFileSync(path.resolve(__dirname, fileName)).toString()
+let counter = 0
+const openingBracket = "("
+const closingBracket = "("
+for (let i = 0; i < inputValues.length; i++) {
+  if (inputValues[i] === openingBracket) {
+    counter++
+  } else {
+    counter--
+  }
+  if (counter === -1) {
+    console.log(i + 1)
+    break
+  }
+}
